81885726c23dc8073e47b80ba83d3da7ffd5736b
daltoj <daltoj@unc.edu>
2024-08-26 10:26:04 -0400

    Excercises

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,1 +1,8 @@
-"""Hello"""
+"""My first excercise in Comp110!"""
+
+__author__ = "730597748"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello," + name + "!"
