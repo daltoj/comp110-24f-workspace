eb57ed222cb086d34c2acdb94c916aa4c2b65d74
daltoj <daltoj@unc.edu>
2024-08-28 09:45:10 -0400

    CQs

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,15 @@
+__author__ = "730597748"
+
+
+def mimic(message: str) -> str:
+    """to copy a message"""
+    return message
+
+
+def main() -> None:
+    """Pulls together my functions"""
+    print(mimic(message=input("Learning code")))
+
+
+if __name__ == "__main__":
+    main()
